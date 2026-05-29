059f79c48b9bcee2466490f52dc36efa8af3b33a
weterynarzfred <weterynarzfred@gmail.com>
2026-05-29 12:02:48 +0200

    styling and debug

--- src/Components/Choice.jsx
+++ src/Components/Choice.jsx
@@ -12,7 +12,7 @@ export default function Choice({ correct, explanation, children, index, selected
   >
     <span className="Choice__marker" />
     <span className="Choice__label">{children}</span>
-    {checked && explanation &&
+    {checked && explanation && (correct || selected) &&
       <p className="Choice__explanation">{explanation}</p>}
   </div>;
 }
--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import mdx from "@mdx-js/rollup";
 import remarkGfm from "remark-gfm";
+import remarkSmartypants from "remark-smartypants";
 import rehypeSlug from "rehype-slug";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     mdx({
-      remarkPlugins: [remarkGfm],
+      remarkPlugins: [remarkGfm, remarkSmartypants],
       rehypePlugins: [rehypeSlug],
       providerImportSource: "@mdx-js/react",
     }),
