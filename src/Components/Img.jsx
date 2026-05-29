e72eec76ac2c237f63d8ddafdc8ee3634ef91f14
weterynarzfred <weterynarzfred@gmail.com>
2026-05-29 23:23:39 +0200

    styling

--- src/Components/Img.jsx
+++ src/Components/Img.jsx
@@ -1,19 +1,21 @@
 import classNames from "classnames";
 
-export default function Img({ src, alt = "", float, width, fullBleed, noMaxHeight, eager }) {
+export default function Img({ src, alt = "", float, width, fullBleed, noMaxHeight, eager, caption }) {
   const style = {};
   if (width && !fullBleed) style.maxWidth = `${width}%`;
 
-  return <img
-    src={src}
-    alt={alt}
-    loading={eager ? "eager" : "lazy"}
-    className={classNames("Img", {
-      "Img--float-left": float === "left",
-      "Img--float-right": float === "right",
-      "Img--full-bleed": fullBleed,
-      "Img--no-max-height": noMaxHeight,
-    })}
-    style={Object.keys(style).length ? style : undefined}
-  />;
+  return (
+    <figure
+      className={classNames("Img", {
+        "Img--float-left": float === "left",
+        "Img--float-right": float === "right",
+        "Img--full-bleed": fullBleed,
+        "Img--no-max-height": noMaxHeight,
+      })}
+      style={Object.keys(style).length ? style : undefined}
+    >
+      <img src={src} alt={alt} loading={eager ? "eager" : "lazy"} className="Img__image" />
+      {caption && <figcaption className="Img__caption">{caption}</figcaption>}
+    </figure>
+  );
 }
